8de8b37dd8f06c63845ec9e7a30ad8d19c44ed88
Diezma <alexisuguzman11@gmail.com>
2024-08-19 00:39:30 -0600

    Completeed Project

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,190 @@
-'use strict';
+"use strict";
+
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+// ----------------------------------------------------------------
+// Mongoose Stock Schema and Model Definition
+// ----------------------------------------------------------------
+
+const stockSchema = new mongoose.Schema({
+	stock: { type: String, required: true },
+	price: { type: Number, required: true },
+	likes: { type: Number, default: 0 },
+	ipsLiked: { type: [String], default: [] },
+});
+const Stock = new mongoose.model("Stock", stockSchema);
+
+// ----------------------------------------------------------------
+// Ip related functions
+// ----------------------------------------------------------------
+
+const hashIp = async (ip) => {
+	const saltRounds = 11;
+	try {
+		const hashedIp = await bcrypt.hash(ip, saltRounds);
+		return hashedIp;
+	} catch (err) {
+		console.error("Error hashing IP:", err);
+		return null;
+	}
+};
+
+const getHashedIps = async (query = {}) => {
+	try {
+		const savedIps = await Stock.find(query).select("ipsLiked -_id");
+		console.log("Saved ips: ", savedIps);
+		const hashedIpsArray = savedIps.map((doc) => doc.ipsLiked).flat();
+		console.log("Hashed ips array: ", hashedIpsArray);
+		return hashedIpsArray;
+	} catch (err) {
+		console.error("Error fetching hashed IPs:", err);
+	}
+};
+
+const isIpSaved = async (ip, hashedIpsArray) => {
+	if (hashedIpsArray == [undefined]) {
+		console.log("Ip is not saved");
+		return false;
+	}
+	console.log("Checking all ips...");
+	for (let hashedIp of hashedIpsArray) {
+		if (await bcrypt.compare(ip, hashedIp)) {
+			return true;
+		}
+	}
+	return false;
+};
+
+// ----------------------------------------------------------------
+// Stock related functions
+// ----------------------------------------------------------------
+
+const saveOrUpdateStock = async (symbol) => {
+	try {
+		const price = await getPrice(symbol);
+		const stock = await Stock.findOneAndUpdate(
+			{ stock: symbol },
+			{ price: price },
+			{ upsert: true, new: true }
+		);
+		console.log("Saved/Updated stock to database:", stock);
+		return stock;
+	} catch (err) {
+		console.error("Error saving stock: ", err);
+	}
+};
+
+const getPrice = async (symbol) => {
+	// API url
+	const url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${symbol}/quote`;
+
+	// fetch stock price
+	try {
+		const response = await fetch(url);
+		const json = await response.json();
+		const stockPrice = json.latestPrice;
+		console.log("Type of price:", typeof stockPrice);
+		return stockPrice;
+	} catch (err) {
+		console.log("Error fetching stock: ", err);
+	}
+};
+
+const singleStockAndLikes = async (symbol, ip, like) => {
+	//Create/Update stock document
+	let stock = await saveOrUpdateStock(symbol);
+
+	//Check if like stock
+	if (like == "true") {
+		stock = await likeStock(stock, ip);
+	}
+
+	const stockObject = {
+		stockData: {
+			stock: stock.stock,
+			price: stock.price,
+			likes: stock.likes,
+		},
+	};
+	console.log(stockObject);
+	return stockObject;
+};
+
+const compareAndRelativeLikes = async (symbols, ip, likes) => {
+	try {
+		//Process stock 1
+		const stock1 = await singleStockAndLikes(symbols[0], ip, likes);
+		//Process stock 2
+		const stock2 = await singleStockAndLikes(symbols[1], ip, likes);
+		//Build stock object response
+
+		const stocksObject = {
+			stockData: [
+				{
+					stock: stock1.stockData.stock,
+					price: stock1.stockData.price,
+					rel_likes: stock2.stockData.likes - stock1.stockData.likes,
+				},
+				{
+					stock: stock2.stockData.stock,
+					price: stock2.stockData.price,
+					rel_likes: stock1.stockData.likes - stock2.stockData.likes,
+				},
+			],
+		};
+		console.log(stocksObject);
+		return stocksObject;
+	} catch (err) {
+		console.error("Error fetching stocks to compare: ", err);
+	}
+};
+
+const likeStock = async (stock, ip) => {
+	try {
+		const query = { stock: stock.stock };
+		const stockLikedIps = await getHashedIps(query);
+		const ipLiked = await isIpSaved(ip, stockLikedIps);
+		if (ipLiked) {
+			console.log("Ip has already liked this stock");
+			return stock;
+		}
+		let hashedIp = await hashIp(ip);
+
+		const likedStock = await Stock.findOneAndUpdate(
+			{ stock: stock.stock },
+			{ $inc: { likes: 1 }, $push: { ipsLiked: hashedIp } },
+			{ new: true }
+		);
+		console.log("Liked stock: ", likedStock);
+		return likedStock;
+	} catch (err) {
+		console.error("Error liking stock: ", err);
+	}
+};
+
+// ----------------------------------------------------------------
+// GET Route
+// ----------------------------------------------------------------
 
 module.exports = function (app) {
+	app.route("/api/stock-prices").get(async function (req, res) {
+		// Variables for ip encryption
+		let ip = req.ip;
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+		//Check for single price and likes or compare and relative likes
+		if (req.query.stock.length == 2) {
+			console.log("Comparing stocks: ", req.query.stock);
+			const symbols = req.query.stock.map((symbol) => symbol);
+			const likes = req.query.like;
+			console.log("Likes: ", req.query.like);
+			const stocksObject = await compareAndRelativeLikes(symbols, ip, likes);
+			res.json(stocksObject);
+		} else {
+			const symbol = req.query.stock;
+			const like = req.query.like;
+			const stockObject = await singleStockAndLikes(symbol, ip, like);
+			res.json(stockObject);
+		}
+	});
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,96 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+	test("Viewing one stock: GET request to /api/stock-prices/", (done) => {
+		chai
+			.request(server)
+			.get("/api/stock-prices/")
+			.query({ stock: "AAPL", like: "false" })
+			.end((err, res) => {
+				assert.equal(res.status, 200);
+				assert.isObject(res.body);
+				assert.property(res.body, "stockData");
+				assert.property(res.body.stockData, "stock");
+				assert.property(res.body.stockData, "price");
+				assert.property(res.body.stockData, "likes");
+			});
+		done();
+	});
 
+	test("Viewing one stock and liking it: GET request to /api/stock-prices/", (done) => {
+		chai
+			.request(server)
+			.get("/api/stock-prices/")
+			.query({ stock: "AAPL", like: "true" })
+			.end((err, res) => {
+				assert.equal(res.status, 200);
+				assert.isObject(res.body);
+				assert.property(res.body, "stockData");
+				assert.property(res.body.stockData, "stock");
+				assert.property(res.body.stockData, "price");
+				assert.property(res.body.stockData, "likes");
+				assert.equal(res.body.stockData.likes, 1);
+			});
+		done();
+	});
+
+	test("Viewing the same stock and liking it again: GET request to /api/stock-prices/", (done) => {
+		chai
+			.request(server)
+			.get("/api/stock-prices/")
+			.query({ stock: "AAPL", like: "true" })
+			.end((err, res) => {
+				assert.equal(res.status, 200);
+				assert.isObject(res.body);
+				assert.property(res.body, "stockData");
+				assert.property(res.body.stockData, "stock");
+				assert.property(res.body.stockData, "price");
+				assert.property(res.body.stockData, "likes");
+				assert.equal(res.body.stockData.likes, 1);
+			});
+		done();
+	});
+
+	test("Viewing two stocks: GET request to /api/stock-prices/", (done) => {
+		chai
+			.request(server)
+			.get("/api/stock-prices/")
+			.query({ stock: ["GOOG", "MSFT"], like: "false" })
+			.end((err, res) => {
+				assert.equal(res.status, 200);
+				assert.isObject(res.body);
+				assert.property(res.body, "stockData");
+				assert.property(res.body.stockData[0], "stock");
+				assert.property(res.body.stockData[0], "price");
+				assert.property(res.body.stockData[0], "rel_likes");
+				assert.property(res.body.stockData[1], "stock");
+				assert.property(res.body.stockData[1], "price");
+				assert.property(res.body.stockData[1], "rel_likes");
+			});
+		done();
+	});
+
+	test("Viewing two stocks and liking them: GET request to /api/stock-prices/", (done) => {
+		chai
+			.request(server)
+			.get("/api/stock-prices/")
+			.query({ stock: ["GOOG", "MSFT"], like: "true" })
+			.end((err, res) => {
+				assert.equal(res.status, 200);
+				assert.isObject(res.body);
+				assert.property(res.body, "stockData");
+				assert.property(res.body.stockData[0], "stock");
+				assert.property(res.body.stockData[0], "price");
+				assert.property(res.body.stockData[0], "rel_likes");
+				assert.property(res.body.stockData[1], "stock");
+				assert.property(res.body.stockData[1], "price");
+				assert.property(res.body.stockData[1], "rel_likes");
+			});
+		done();
+	});
 });
